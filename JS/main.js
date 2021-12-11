dee89de16655e8840fb859084ec42a86b2018672
Laura Daniela Benavides de los Ríos <82069691+LauraBenavidesdlrv@users.noreply.github.com>
2021-12-11 16:34:58 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -7,24 +7,17 @@ window.addEventListener('load', () => {
                 precio:null,
                 tiempo:null
             }
-        },
-        created: function(){
-            axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then((val) => {
+     },
+     created: function(){
+          axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then((val) => {
                 this.bitcoinData.precio = val.data.bpi.USD.rate_float 
                 this.bitcoinData.tiempo = val.data.time.updated
                 console.log(this.bitcoinData)
-            })
-
+           })
             .catch((err) => {
                 console.log(err)
-
-            })
-                
-
+            })             
         }
-
-
     })
-
 }) 
